9886adcff2a3614ce92ea0e679e2002d93929c09
Jordan Yee <jordanyee799@gmail.com>
2021-10-14 00:53:36 -0700

    commit tree 64caa2778ebcef4a817b06ef0a1140ee07413716

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(function (name) {
+        return name.toLowerCase() === str.toLowerCase();
+    });
+}
+
+function fuzzyMatch(names, str) {
+    return names.filter(function (name) {
+        return name.slice(0, str.length) === str;
+    })
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(obj => obj.name === str)
+}
